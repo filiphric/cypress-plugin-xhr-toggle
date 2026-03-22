2df24d8a767dcbbf1e1534d89455e7b09126d382
Filip Hric <filip@filiphric.sk>
2026-03-22 14:29:01 +0100

    chore: remove @replayio/cypress dependency

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from "cypress";
-import { plugin as replayPlugin } from "@replayio/cypress";
 
 export default defineConfig({
   fixturesFolder: false,
-  e2e: {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    setupNodeEvents(on, config) {
-      replayPlugin(on, config, {
-        upload: true,
-        apiKey: process.env.REPLAY_API_KEY,
-      });
-      return config;
-     },
-  },
+  e2e: {},
 });
